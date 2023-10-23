63a253cb6b034bb0ca33e4b8a12b5c902bb1aa60
Simontagbor <simontagbor360@gmail.com>
2023-10-23 06:12:13 -0700

    fix pycodestyle error

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,11 +5,11 @@
 def index_range(page, page_size):
     """ Return a tuple of size two containing a
     start index and an end index corresponding
-    
+
     Args:
         page (int): page number
         page_size (int): size of the page
-    
+
     Returns:
         tuple: start index and end index
     """
